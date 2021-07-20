cef59c9f429fc321123237172f173961fa95d232
Leandro Ferroni <ferroni.leandro@gmail.com>
2021-07-20 00:24:21 -0300

    Ends interfaces and api requests

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container } from './styles'
 
-interface Transaction {
-    id: number,
-    title: string,
-    type: string,
-    category: string,
-    amount: number,
-    createdAt: string
-}
-
 export function TransactionsTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
-    }, []);
+    const { transactions } = useTransactions();
 
     return (
         <Container>
@@ -31,7 +16,7 @@ export function TransactionsTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    {transactions.map((transaction: Transaction) => (
+                    {transactions.map((transaction) => (
                         <tr key={transaction.id}>
                             <td>{transaction.title}</td>
                             <td className={transaction.type}>
